2438a9b7f4ea5feb170eacbdaee2b78ab94de6d6
sawyer burnett <sawyerburnett@gmail.com>
2016-12-22 21:55:52 -0700

    adding status result to pagegit status!

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -4,17 +4,20 @@ var myApp = angular.module('MainCtrl', []);
 myApp.controller('MainController', ['$scope', '$http', function($scope, $http) {
 
 	$scope.pauseSonos = function () {
-		$http.get('http://localhost:5005/master%20room/pause')
-       .then(function (response) { 
+		$http.get('http://localhost:5005/master%20room/pause').
+       	then(function (response) { 
        		$scope.sonosResult = response.data;
-       });
-
+        }, function (response) {
+       		$scope.sonosResult = response.data;
+        });
 	};
 	$scope.playSonos = function() {
-		$http.get('http://localhost:5005/master%20room/play')
-        .then(function (response) {
-        	$scope.sonosResult = response.data;
-    	});
+		$http.get('http://localhost:5005/master%20room/play').
+		then(function (response) { 
+	   		$scope.sonosResult = response.data;
+	    }, function (response) {
+		   	$scope.sonosResult = response.data;
+	   });
 	};
 
 
