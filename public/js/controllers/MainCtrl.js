3434091a5e39f13883be93d165efa6061c3af06a
sawyer burnett <sawyerburnett@gmail.com>
2016-12-22 21:47:59 -0700

    switching to use angular http get calls so that the page doesn't navigate somewhere else

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,23 @@
 // public/js/controllers/MainCtrl.js
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+var myApp = angular.module('MainCtrl', []);
+
+myApp.controller('MainController', ['$scope', '$http', function($scope, $http) {
+
+	$scope.pauseSonos = function () {
+		$http.get('http://localhost:5005/master%20room/pause')
+       .then(function (response) { 
+       		$scope.sonosResult = response.data;
+       });
+
+	};
+	$scope.playSonos = function() {
+		$http.get('http://localhost:5005/master%20room/play')
+        .then(function (response) {
+        	$scope.sonosResult = response.data;
+    	});
+	};
+
 
     $scope.tagline = 'Sonos Controls';   
 
-});
+}]);
