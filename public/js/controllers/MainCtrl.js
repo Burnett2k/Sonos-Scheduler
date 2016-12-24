f3fbb6615703af4bd89225681b64fb6e0b4445e7
Sawyer Burnett <sawyerburnett@gmail.com>
2016-12-23 23:42:49 -0600

    add fix to set status result correctly upon error with http call

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -6,33 +6,34 @@ myApp.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	$scope.previousTrack = function () {
 		$http.get('http://localhost:5005/master%20room/previous').
        	then(function (response) { 
-       		$scope.sonosResult = response.data;
+       		$scope.sonosResult = response.data.status;
         }, function (response) {
-       		$scope.sonosResult = response.data;
+        	console.log(response);
+       		$scope.sonosResult = 'error';
         });
 	};
 	$scope.pauseSonos = function () {
 		$http.get('http://localhost:5005/master%20room/pause').
        	then(function (response) { 
-       		$scope.sonosResult = response.data;
+       		$scope.sonosResult = response.data.status;
         }, function (response) {
-       		$scope.sonosResult = response.data;
+       		$scope.sonosResult = 'error';
         });
 	};
 	$scope.playSonos = function() {
 		$http.get('http://localhost:5005/master%20room/play').
 		then(function (response) { 
-	   		$scope.sonosResult = response.data;
+	   		$scope.sonosResult = response.data.status;
 	    }, function (response) {
-		   	$scope.sonosResult = response.data;
+		   	$scope.sonosResult = 'error';
 	   });
 	};
 	$scope.nextTrack = function () {
 		$http.get('http://localhost:5005/master%20room/next').
        	then(function (response) { 
-       		$scope.sonosResult = response.data;
+       		$scope.sonosResult = response.data.status;
         }, function (response) {
-       		$scope.sonosResult = response.data;
+       		$scope.sonosResult = 'error';
         });
 	};
 
